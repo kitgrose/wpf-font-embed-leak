094be1e9b4be5b15360623a8b1305ff62aa8b222
Kit Grose <kit@studioiq.com.au>
2023-02-20 17:02:59 +1100

    Add demonstration app to repo
    I've switched the example to use just open-source fonts to make it
    possible to share the repo with the WPF project maintainers.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+namespace TestWpfFontEmbedsDotNet7
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            var resourceEnumerator = Application.Current.Resources.GetEnumerator();
+            var fontChanger = new DispatcherTimer();
+            fontChanger.Tick += (s, e) =>
+            {
+                if (!resourceEnumerator.MoveNext())
+                {
+                    resourceEnumerator.Reset();
+                    resourceEnumerator.MoveNext();
+                }
+
+                if (resourceEnumerator.Value is FontFamily fontFamily)
+                {
+                    TestBlock.FontFamily = fontFamily;
+                }
+            };
+            fontChanger.Start();
+        }
+    }
+}
